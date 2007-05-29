6c3a904cb19ccb390bc8267194dd5b18afee893b
oczykota <oczykota@e3ae7234-a431-0410-a736-3bfa2c0b22a5>
2007-05-29 23:39:37 +0000

    Rozpoczęcie projektu.

---
+++ trunk/JEbeno3D/src/jebeno3d/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 30 maj 2007, 01:42
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package jebeno3d;
+
+/**
+ *
+ * @author arkadiusz
+ */
+public class Main {
+  
+  /** Creates a new instance of Main */
+  public Main() {
+  }
+  
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO code application logic here
+  }
+  
+}
