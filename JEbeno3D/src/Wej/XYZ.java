eedc7ab469e4f68863d3666ba8c0d47436e5ccb5
oczykota <oczykota@e3ae7234-a431-0410-a736-3bfa2c0b22a5>
2007-06-01 11:12:53 +0000

    ""

---
+++ JEbeno3D/src/Wej/XYZ.java
@@ -0,0 +1,18 @@
+/*
+ * XYZ.java
+ *
+ * Created on 1 czerwiec 2007, 13:17
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package Wej;
+
+/**
+ *
+ * @author arkadiusz
+ */
+public interface XYZ {
+   
+}
